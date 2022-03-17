885397794a5454909a4fc22775d672e6cb4062d2
21people <dnjsaud0427@naver.com>
2022-03-17 15:52:39 +0900

    react-query 배우기 전

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { ThemeProvider } from "styled-components";
 import App from "./App";
-import { lightTheme } from "./Theme";
+import { theme } from "./theme";
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={lightTheme}>
+    <ThemeProvider theme={theme}>
       <App />
     </ThemeProvider>
   </React.StrictMode>,
