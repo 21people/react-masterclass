f3257704dcb792cac0f8b91958e820eceb6cf17e
21people <dnjsaud0427@naver.com>
2022-04-11 17:08:10 +0900

    selector part 1

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,89 +1,36 @@
-import React, { useState } from "react";
-import { useForm } from "react-hook-form";
-import {
-  atom,
-  DefaultValue,
-  useRecoilState,
-  useRecoilValue,
-  useSetRecoilState,
-} from "recoil";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { toDoSelector, toDoState } from "./atoms";
+import CreateToDo from "./CreateToDo";
+import ToDo from "./ToDo";
 
-// function ToDoList() {
-//   const [toDo, setToDo] = useState("");
-//   const onChange = (e: React.FormEvent<HTMLInputElement>) => {
-//     const {
-//       currentTarget: { value },
-//     } = e;
-//     setToDo(value);
-//   };
-//   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-//     e.preventDefault();
-//     setToDo("");
-//   };
-//   return (
-//     <div>
-//       <form onSubmit={onSubmit}>
-//         <input placeholder="Write a to do" onChange={onChange} value={toDo} />
-//         <button type="submit">Add</button>
-//       </form>
-//     </div>
-//   );
-// }
-interface IToDo {
-  text: string;
-  category: "DONE" | "DOING" | "TO_DO";
-}
-const toDoState = atom<IToDo[]>({
-  key: "toDo",
-  default: [],
-});
-interface IForm {
-  toDo?: string;
-  extraError?: string;
-}
 function ToDoList() {
-  const [toDos, setToDos] = useRecoilState(toDoState);
-
-  const {
-    register,
-    handleSubmit,
-    setValue,
-    formState: { errors },
-    setError,
-  } = useForm<IForm>({ defaultValues: { toDo: "I have to do" } });
-  const handleValid = (data: IForm) => {
-    if (!data.toDo?.includes("I have to do")) {
-      setError("toDo", { message: "Write Exact ToDo" }, { shouldFocus: true });
-    }
-    setValue("toDo", `I have to do`);
-    // setError("extraError", { message: "Server is offline" });
-  };
+  const [toDo, doing, done] = useRecoilValue(toDoSelector);
   return (
     <div>
       <h1>To Dos</h1>
       <hr />
-      <form onSubmit={handleSubmit(handleValid)}>
-        <input
-          {...register("toDo", {
-            required: "toDo is required",
-            minLength: { value: 10, message: "Too short!" },
-            // pattern: {
-            //   value: /^[A-Za-z0-9._%+-]+@naver.com$/,
-            //   message: "Only naver.com email allowed",
-            // },
-            validate: {
-              noNico: (value) =>
-                value?.includes("nico") ? "No nicos allowed" : true,
-              noNick: (value) =>
-                value?.includes("nick") ? "No nicks allowed" : true,
-            },
-          })}
-          placeholder="Write a to do"
-        />
-        <span>{errors?.toDo?.message}</span>
-        <button>Add</button>
-        <span>{errors?.extraError?.message}</span>
-      </form>
+      <CreateToDo />
+      <hr />
+      <h2>ToDo</h2>
+      <ul>
+        {toDo.map((toDo) => (
+          <ToDo key={toDo.id} {...toDo} />
+        ))}
+      </ul>
+      <hr />
+      <h2>Doing</h2>
+      <ul>
+        {doing.map((toDo) => (
+          <ToDo key={toDo.id} {...toDo} />
+        ))}
+      </ul>
+      <hr />
+      <h2>Done</h2>
+      <ul>
+        {done.map((toDo) => (
+          <ToDo key={toDo.id} {...toDo} />
+        ))}
+      </ul>
     </div>
   );
 }
