96c3cb1cb495559d690ac665eaa55ccfc2ea8d42
21people <dnjsaud0427@naver.com>
2022-04-15 15:21:44 +0900

    gestures

--- src/App.tsx
+++ src/App.tsx
@@ -12,58 +12,18 @@ const Wrapper = styled.div`
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  background-color: rgba(255, 255, 255, 0.2);
-  border-radius: 50px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  grid-template-rows: repeat(2, 1fr);
-  gap: 20px;
-
-  padding: 20px;
-`;
-const Circle = styled(motion.div)`
-  width: 100%;
-  height: 100%;
   background-color: white;
-  border-radius: 50%;
+  border-radius: 50px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 const boxVars = {
-  start: {
-    opacity: 0,
-    scale: 0.5,
-    y: 10,
-  },
-  end: {
-    opacity: 1,
-    scale: 1,
-    transition: {
-      type: "spring",
-      duration: 0.5,
-      bounce: 0.5,
-      delayChildren: 0.5,
-      staggerChildren: 0.1,
-    },
-    y: 0,
-  },
-};
-const circleVars = {
-  start: {
-    opacity: 0,
-    y: 10,
-  },
-  end: { opacity: 1, y: 0 },
+  hover: { rotateZ: 90, scale: 1.2, transition: { type: "spring" } },
+  tap: { scale: 0.8, borderRadius: "50%" },
 };
 function App() {
   return (
     <Wrapper>
-      <Box variants={boxVars} initial="start" animate="end">
-        <Circle variants={circleVars} />
-        <Circle variants={circleVars} />
-        <Circle variants={circleVars} />
-        <Circle variants={circleVars} />
-      </Box>
+      <Box variants={boxVars} whileHover="hover" whileTap="tap" />
     </Wrapper>
   );
 }
