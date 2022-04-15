42973cf0240d788967d901b79aa1f2228e1f1c85
21people <dnjsaud0427@naver.com>
2022-04-15 15:42:06 +0900

    drag Motion

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { motion } from "framer-motion";
+import { useRef } from "react";
 
 const Wrapper = styled.div`
   width: 100vw;
@@ -9,21 +10,45 @@ const Wrapper = styled.div`
   align-items: center;
   background-color: transparent;
 `;
+const BiggerBox = styled.div`
+  width: 170px;
+  height: 170px;
+  background-color: rgba(255, 255, 255, 0.3);
+  border-radius: 25%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  overflow: hidden;
+`;
 const Box = styled(motion.div)`
-  width: 200px;
-  height: 200px;
+  width: 100px;
+  height: 100px;
   background-color: white;
-  border-radius: 50px;
+  border-radius: 15%;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 const boxVars = {
-  hover: { rotateZ: 90, scale: 1.2, transition: { type: "spring" } },
-  tap: { scale: 0.8, borderRadius: "50%" },
+  // hover: { rotateZ: 90, scale: 1.2, transition: { type: "spring" } },
+  // tap: { scale: 0.8, borderRadius: "50%" },
+  drag: { backgroundColor: "rgb(46,204,113)", transition: { duration: 10 } },
 };
 function App() {
+  const biggerBoxRef = useRef<HTMLDivElement>(null);
   return (
     <Wrapper>
-      <Box variants={boxVars} whileHover="hover" whileTap="tap" />
+      <BiggerBox ref={biggerBoxRef}>
+        <Box
+          drag
+          dragSnapToOrigin
+          dragElastic={0.5}
+          dragConstraints={biggerBoxRef}
+          variants={boxVars}
+          whileHover="hover"
+          whileTap="tap"
+          whileDrag="drag"
+        />
+      </BiggerBox>
     </Wrapper>
   );
 }
