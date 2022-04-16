5d49bcc8b783940d8a89a74abcadee4c981e0dea
21people <dnjsaud0427@naver.com>
2022-04-16 14:44:42 +0900

    slider

--- src/App.tsx
+++ src/App.tsx
@@ -9,31 +9,64 @@ const Wrapper = styled(motion.div)`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  background: linear-gradient(135deg, rgb(238, 0, 153), rgb(221, 0, 238)); ;
+  background: linear-gradient(135deg, rgb(238, 0, 153), rgb(221, 0, 238));
 `;
 const Box = styled(motion.div)`
+  top: 180px;
+  position: absolute;
   width: 200px;
   height: 150px;
   background-color: white;
   margin-bottom: 30px;
-  border-radius: 15%;
+  border-radius: 20%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 28px;
+  margin: 20px;
 `;
 const boxVars = {
-  initial: { opacity: 0, scale: 0 },
-  animate: { opacity: 1, scale: 1, rotateZ: 360 },
-  exit: { opacity: 0, scale: 0, y: 50 },
+  entry: (back: boolean) => ({ x: back ? 500 : -500, opacity: 0, scale: 1 }),
+  center: {
+    x: 0,
+    opacity: 1,
+    scale: 1,
+    transition: { duration: 0.3 },
+  },
+  exit: (back: boolean) => ({
+    x: back ? -500 : 500,
+    opacity: 0,
+    scale: 1,
+    transition: { duration: 0.3 },
+  }),
 };
 function App() {
-  const [showing, setShowing] = useState(false);
-  const toggleShowing = () => {
-    setShowing((prev) => !prev);
+  const [visible, setVisible] = useState(1);
+  const [back, setBack] = useState(false);
+  const former = () => {
+    setVisible((prev) => (prev !== 1 ? --prev : 1));
+    setBack(false);
+  };
+  const latter = () => {
+    setVisible((prev) => (prev !== 10 ? ++prev : 10));
+    setBack(true);
   };
   return (
     <Wrapper>
-      <AnimatePresence>
-        {showing ? <Box variants={boxVars} {...boxVars} /> : null}
+      <AnimatePresence exitBeforeEnter custom={back}>
+        <Box
+          custom={back}
+          key={visible}
+          variants={boxVars}
+          initial="entry"
+          animate="center"
+          exit="exit"
+        >
+          {visible}
+        </Box>
       </AnimatePresence>
-      <button onClick={toggleShowing}>Click Me</button>
+      <button onClick={former}>Former</button>
+      <button onClick={latter}>Latter</button>
     </Wrapper>
   );
 }
