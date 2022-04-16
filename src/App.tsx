7cb25ae6a64c6943f2935ecb72d5f6c5648bdb8a
21people <dnjsaud0427@naver.com>
2022-04-16 15:09:36 +0900

    layout standard

--- src/App.tsx
+++ src/App.tsx
@@ -12,61 +12,35 @@ const Wrapper = styled(motion.div)`
   background: linear-gradient(135deg, rgb(238, 0, 153), rgb(221, 0, 238));
 `;
 const Box = styled(motion.div)`
-  top: 180px;
-  position: absolute;
-  width: 200px;
-  height: 150px;
-  background-color: white;
-  margin-bottom: 30px;
-  border-radius: 20%;
+  width: 400px;
+  height: 400px;
+  background-color: rgba(255, 255, 255, 1);
+  border-radius: 12.5%;
   display: flex;
   justify-content: center;
   align-items: center;
-  font-size: 28px;
-  margin: 20px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
-const boxVars = {
-  entry: (back: boolean) => ({ x: back ? 500 : -500, opacity: 0, scale: 1 }),
-  center: {
-    x: 0,
-    opacity: 1,
-    scale: 1,
-    transition: { duration: 0.3 },
-  },
-  exit: (back: boolean) => ({
-    x: back ? -500 : 500,
-    opacity: 0,
-    scale: 1,
-    transition: { duration: 0.3 },
-  }),
-};
+const Circle = styled(motion.div)`
+  width: 100px;
+  height: 100px;
+  background-color: #00a5ff;
+  border-radius: 50%;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0 6);
+`;
 function App() {
-  const [visible, setVisible] = useState(1);
-  const [back, setBack] = useState(false);
-  const former = () => {
-    setVisible((prev) => (prev !== 1 ? --prev : 1));
-    setBack(false);
-  };
-  const latter = () => {
-    setVisible((prev) => (prev !== 10 ? ++prev : 10));
-    setBack(true);
-  };
+  const [clicked, setClicked] = useState(false);
+  const toggleClicked = () => setClicked((prev) => !prev);
   return (
-    <Wrapper>
-      <AnimatePresence exitBeforeEnter custom={back}>
-        <Box
-          custom={back}
-          key={visible}
-          variants={boxVars}
-          initial="entry"
-          animate="center"
-          exit="exit"
-        >
-          {visible}
-        </Box>
-      </AnimatePresence>
-      <button onClick={former}>Former</button>
-      <button onClick={latter}>Latter</button>
+    <Wrapper onClick={toggleClicked}>
+      <Box
+        style={{
+          justifyContent: clicked ? "flex-start" : "center",
+          alignItems: clicked ? "flex-start" : "center",
+        }}
+      >
+        <Circle layout />
+      </Box>
     </Wrapper>
   );
 }
